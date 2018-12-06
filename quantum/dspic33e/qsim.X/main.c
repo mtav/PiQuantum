d311991e19504e43620ade4b449be90e72f77af9
John Scott <johnrscott0@gmail.com>
2018-12-06 22:20:51 +0000

    Made a new project for the simulator

---
+++ quantum/dspic33e/qsim.X/main.c
@@ -0,0 +1,25 @@
+/**
+ * @file main.c
+ * @authors J Scott, O Thomas
+ * @date Dec 2018
+ *
+ * @brief The main function
+ *
+ * @detail Contains the entry point for a 16 qubit quantum computer simulator
+ *
+ * Compile command: make (on linux). But if you want to program the micro-
+ * controller too or if you're using windows you're better of downloading
+ * and installing MPLAB-X https://www.microchip.com/mplab/mplab-x-ide.
+ *
+ * @note You also need the microchip xc16 compilers which
+ * are available from https://www.microchip.com/mplab/compilers 
+ *
+ */
+#include "config.h"
+
+int main(void) {
+
+
+    while (1); ///< @note Really important!
+    return 0;
+}
