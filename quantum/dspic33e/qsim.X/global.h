52a552f074cc6b2f29e52ccea392370ec57ed1f4
John Scott <johnrscott0@gmail.com>
2018-12-06 23:47:29 +0000

    Added a global.h file

---
+++ quantum/dspic33e/qsim.X/global.h
@@ -0,0 +1,44 @@
+/* 
+ * @file:   global.h
+ * @authors: Oliver Thomas and John Scott
+ *
+ * @date Created on 06 December 2018, 23:36
+ * 
+ * @detail This file contains the global variable used by algorithms and
+ * `simulator applications' to control the manipulation and display of the
+ * state vector.
+ * 
+ * The variable has certain `input' variables:
+ * 1) to specify single and controlled qubit operations to be performed
+ * 2) to buffer several operations
+ * 3) to specify the display mode (average or cycle)
+ * 4) to request a display update
+ * 5) to set the qubit `error level'
+ * 6) to turn error correction mode on and off
+ * 7) to perform a (projective) measurement on the qubits
+ * 8) to reset the simulator
+ * 
+ * The variable has certain `output' variables
+ * 1) to read the state of the external buttons
+ * 2) to request the result of measurements
+ * 3) to get the status of various operations
+ * 
+ * 
+ */
+
+#ifndef GLOBAL_H
+#define	GLOBAL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+///
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GLOBAL_H */
+
