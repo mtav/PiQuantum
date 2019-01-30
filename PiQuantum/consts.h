75ff958489b1d2eef7187189264754e25516189f
Oliver <oliver.thomas404@gmail.com>
2019-01-30 15:38:04 +0000

    working on the main structure

---
+++ PiQuantum/consts.h
@@ -0,0 +1,8 @@
+
+
+/// type for glob
+typedef struct {
+    int display[3]; /// display array, [Avg, cycle, Measure]
+    int algorithm[2];   // [gates, subroutines] 
+    int error_correction; // 0 or 1
+    } GLOB;
--- PiQuantum/main.c
+++ PiQuantum/main.c
@@ -1,1 +1,35 @@
+/** 
+ * @file main.c
+ * @authors J Scott, O Thomas
+ * @date Jan 2019 
+ *
+ * @detail main function for the Raspberry Pi Quantum computer simulator.
+ *
+ */
 
+int main(void) {
+    
+    /** glob
+    * ---------------------------------
+    * main flags 
+    *   
+    *   Reset
+    *   Display
+    *       Disp_Average
+    *       Disp_Cycle
+    *       Measure
+    *   Algorithm
+    *       Gates
+    *           Single qubit
+    *           Two qubit
+    *       Subroutines 
+    *   Error correction mode
+    *   QASM
+    *       Read QASM
+    *       Write QASM 
+    */
+    
+
+    int glob[];
+
+}
