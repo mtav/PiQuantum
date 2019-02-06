0b67d9bce3bd476612dc737302a86b7c88275128
John Scott <johnrscott0@gmail.com>
2019-02-06 15:42:38 +0000

    Not fixed SPI yet

--- src/spi.cpp
+++ src/spi.cpp
@@ -14,6 +14,8 @@
 #include <iostream>
 #include <errno.h>
 #include <string.h>
+#include <vector>
+#include <algorithm>
 
 class SpiChannel {
 private:
@@ -32,11 +34,23 @@ public:
 
   // Simultaneous read/write data to the SPI interface
   // Pass a pointer to the buffer, and the length of the buffer
-  void ReadWrite(unsigned char * buffer, int len) {
+  std::vector<unsigned char> read_write(std::vector<unsigned char> write) {
+    // Allocate memory for a data buffer 
+    int len = write.size();
+    unsigned char * buffer =
+      static_cast<unsigned char * >(malloc(sizeof(char)*len));
+    // Store std::vector in char array
+    std::copy(write.begin(), write.end(), buffer);
+    // Send the data
     int result = wiringPiSPIDataRW(channel, buffer, len);
     if(result != len) {
       std::cout << "SPI Error: wrong amount of data sent" << std::endl;
     }
+    // Store returned data in std::vector
+    std::vector<unsigned char> read(buffer, buffer+len);
+    // Free the buffer memory
+    free(buffer);
+    return read;
   }
   
 };
@@ -50,23 +64,34 @@ int main() {
 
   // SPI channel type
   SpiChannel spi(ch, frequency);
+
+  // Data to send
+  std::vector<unsigned char> write = {0,1,2,3};
+
+  // Read and write data
+  std::vector<unsigned char> read = spi.read_write(write);
+
+  // Print results
+  std::cout << "Data sent: " << write[0] << std::endl
+	    << "Data returned: " << read[1] << std::endl;
   
   // Some data to write
-  int len = 4; // Length of buffer
-  unsigned char * buffer = static_cast<unsigned char * >(malloc(sizeof(char)*len));
+  /*
   * (buffer + 0) = 0;
   * (buffer + 1) = 1;
   * (buffer + 2) = 2;
   * (buffer + 3) = 3;
-
+  */
   // Send data
-  spi.ReadWrite(buffer, len);
+  //spi.ReadWrite(buffer, len);
 
   // Data returned
+  /*
   std::cout << "Data read: "
 	    << std::to_string(*(buffer+0))
 	    << std::to_string(*(buffer+1))
 	    << std::to_string(*(buffer+2))
 	    << std::to_string(*(buffer+3))
 	    << std::endl;
+  */
 }
