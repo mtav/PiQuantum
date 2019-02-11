a0a7833e0c3c9ba0b4b5c4c86ee83e2498ccd3c0
Oliver Thomas <oliver.thomas404@gmail.com>
2019-02-11 22:48:39 +0000

    a mess but sorted the passing Eigen matrices to the operator class.  time for illegal overloading :p

--- src/mathematics/consts.hpp
+++ src/mathematics/consts.hpp
@@ -3,146 +3,51 @@
 #include <iostream>
 #include <stdlib.h>
 
+#include "classes.hpp"
+
 typedef std::complex<double> CD;
 
+//double r2=sqrt(2);
+
+/// should be able to pass like this 
+// define ALL OPERATYORS LIKE THIS PLEASE OI FIXED IT
+static Operators X(1, (Eigen::Matrix2cd() << 
+            0,1,
+            1,0).finished()); 
+
+//static Operators X;
+
+//(1, (Eigen::Matrix2cd() << 
+//        0,1,
+//        1,0).finished());
+
+/*
 // 2x2 single qubit gates
-Eigen::Matrix2cd X, Y, Z, H;
+// don't worry about it!
 
+extern Operators X(1);
+X.matrix = (Eigen::Matrix2cd() << 
+        0,1,
+        1,0).finished(); 
+*/
+static Eigen::Matrix2cd Y = (Eigen::Matrix2cd() << 
+        0, CD(0,-1),
+        CD(0,1), 0).finished(); 
+/*
+static Eigen::Matrix2cd Z = (Eigen::Matrix2cd() << 
+        1,0,
+        0,-1).finished(); 
+
+static Eigen::Matrix2cd H = (Eigen::Matrix2cd() << 
+        1/r2, 1/r2,
+        1/r2, 1/r2).finished(); 
+
+// I keep forgetting we don't need these...
 // 4x4 two-qubit gates
-Eigen::Matrix4cd CNOT, CPHASE;
+static Eigen::Matrix4cd CNOT = (Eigen::Matrix4cd() << 
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 0, 1,
+        0, 0, 1, 0).finished(); 
 
-// --------------------- ThE gRaNd StAtE vEcToR cLaSs -----------------
-class State_Vectors {
-    int num_qubits;
-    int size;
-    Eigen::VectorXcd vect;
-
-    public:
-    // ---------------------- methods -----------------------------
-    int get_num_qubits(){return num_qubits;};
-    int get_size(){return size;}; 
-    int get_eigen_size(){return vect.size();}; 
-
-    void print(int pos){std::cout << vect(pos) << std::endl;};
-
-    // ---------------------- WeIrD iNdEx LoOpInG --------------------
-
-    void apply(Eigen::Matrix2cd op, int qubit)
-    {
-        /** apply operator
-         * @param state state vector containing amplitudes 
-         * @param qubit qubit number to apply 2x2 matrix to
-         * @param N total number of qubits in the state
-         * @param op 2x2 operator to be applied
-         * 
-         * This routine applies a single qubit gate to the state vector @param state.
-         * Consider the three qubit case, with amplitudes shown in the table below:
-         *
-         *   \verbatim
-         *     index     binary   amplitude 
-         *      ----------------------------- 
-         *        0       0 0 0       a0
-         *        1       0 0 1       a1 
-         *        2       0 1 0       a2
-         *        3       0 1 1       a3
-         *        4       1 0 0       a4
-         *        5       1 0 1       a5
-         *        6       1 1 0       a6
-         *        7       1 1 1       a7
-         *      -----------------------------
-         *      Qubit:    2 1 0
-         * \endverbatim
-         * 
-         * If a single qubit operation is applied to qubit 2, then the 2x2 matrix 
-         * must be applied to all pairs of (0,1) in the first column, with the numbers
-         * in the other columns fixed. In other words, the following indices are paired:
-         * 
-         *       (0+0) (1+0) (2+0) (3+0)
-         *       (4+0) (5+0) (6+0) (7+0)
-         * 
-         * where the top line corresponds to the ZERO amplitude and the bottom row
-         * corresponds to the ONE amplitude. 
-         * 
-         * Similarly, for qubit 1 the pairings are:
-         * 
-         *       (0+0) (0+4) (1+0) (1+4)
-         *       (2+0) (2+4) (3+0) (3+4)
-         * 
-         * And for qubit 0 the pairings are:
-         * 
-         *       (0+0) (0+2) (0+4) (0+6)
-         *       (1+0) (1+2) (1+4) (1+6)
-         * 
-         * These numbers are exactly the same as the previous function, which means
-         * the same nested loops can be used to perform operation. Now the index
-         * 
-         *      root + step 
-         * 
-         * refers to the ZERO amplitude (the first element in the column vector to
-         * be multiplied by the 2x2 matrix), and the index
-         * Complex state[], int N
-         *      root + 2^k + step
-         * 
-         * corresponds to the ONE entry.
-         *
-         * -----------------------------------------------------------------------
-         * @brief New function to perform single qubit gates
-         * @param op the unitary to perform
-         * @param k the index of the qubit to modify
-         * @param state the state vector
-         * 
-         * The function computes a single qubit operation acting on @param state.
-         * It sets the bit position associated with the qubit to zero, and then 
-         * generates all possible indices in the other bit positions. These 
-         * correspond to the ZERO states. The ONE states are obtained by changing
-         * the kth bit from zero to one.
-         * 
-         */
-        void single_qubit_op_new(const Complex op[2][2], int k, Complex state[]) {
-            int bit = (1 << k); // The bit position corresponding to the kth qubit
-            int high_incr = (bit << 1); 
-            // Increment through the indices above bit
-            for(int i=0; i<STATE_LENGTH; i+=high_incr) {
-                // Increment through the indices less than bit
-                for(int j=0; j<bit; j++) {
-                    // 2x2 matrix multiplication on the zero (i+j)
-                    // and one (i+j+bit) indices
-                    //mat_mul(op, state, i+j, i+j+bit);
-                    vect(i+j) = op(0,0)*vect(i+j) + op(0,1)*vect(i+j+bit);
-                    vect(i+j+bit) = 
-                }
-            }
-        }
-
-    }
-
-    // returns 2 vector,
-    // takes 2x2 matrix
-    // vector 
-    // and selects the i-th and j-th elements from the vector
-    Eigen::Vector2cd mat_mul(Eigen::Matrix2cd op, Eigen::VectorXcd v, int i, int j)
-    {
-        // make temp vector of size 2
-        Eigen::Vector2cd temp;
-        temp(0) = v(i);
-        temp(1) = v(j);
-
-        return op*temp;
-    }
-
-    // ----------------------- constructors --------------------------
-    // default case if no qubits are specified
-    State_Vectors() : State_Vectors(16) { }
-
-    // if one int passed take as number of qubits 
-    State_Vectors(int num_qubits)
-    {
-        size = pow(2, num_qubits);
-        // initialise state vector to zero matrix of correct size
-        vect = Eigen::VectorXcd::Zero(size);
-        // make the first element 1 (Vacuum state)
-        vect(0)=CD(1.0,0.0);
-    }
-};
-
-
+*/
--- src/mathematics/math_test.cpp
+++ src/mathematics/math_test.cpp
@@ -3,18 +3,49 @@
 #include <stdio.h>
 
 #include <iostream>
+
 #include "consts.hpp"
+
 
 int main(void)
 {
     // call constructor 
     State_Vectors state(2);
 
-    for(int i=0; i<state.get_size(); i++) state.print(i);
-    
-    std::cout << state.get_size() << std::endl;
+    std::cout << "State vector is" <<std::endl;
+    state.print();
+
+    std::cout << "size " << state.get_size() << std::endl;
     // std::cout << state.get_eigen_size() << std::endl;
 
+
+    // not working...
+   //Operators X(1, Y);
+
+
+/*    std::cout << "Single qubit gates, X, Y, Z, H " <<std::endl;
+    std::cout << X <<std::endl;
+    std::cout << Y <<std::endl;
+    std::cout << Z <<std::endl;
+    std::cout << H <<std::endl;
+
+
+    state.single_qubit_op(H,0); 
+    state.print();
+    
+    state.single_qubit_op(Z,0);
+    state.print();
+*/
+//    make_op();
+    //operator_assignment();
+    X.print();
+    std::cout << X.get_num_qubits() <<std::endl;
+
+    std::cout << typeid(X.matrix).name() <<std::endl;
+
+    std::cout << typeid((Eigen::Matrix2cd() << 
+        0, CD(0,-1),
+        CD(0,1), 0).finished()).name() <<std::endl;
 }
 
 
