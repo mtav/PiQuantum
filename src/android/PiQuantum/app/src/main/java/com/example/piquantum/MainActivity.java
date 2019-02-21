66b222bb8b5c5c74912f6f64c742b408a00dcd19
John Scott <johnrscott0@gmail.com>
2019-02-21 14:41:26 +0000

    Bluetooth tests

--- src/android/PiQuantum/app/src/main/java/com/example/piquantum/MainActivity.java
+++ src/android/PiQuantum/app/src/main/java/com/example/piquantum/MainActivity.java
@@ -10,6 +10,8 @@ import android.content.IntentFilter;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.os.Parcelable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView paired;
     private TextView discovered;
     private TextView connecting;
+    private TextView uuids;
     private Button connect;
     //private Connect connect;
 
@@ -45,11 +48,16 @@ public class MainActivity extends AppCompatActivity {
         paired = (TextView) findViewById(R.id.paired);
         discovered = (TextView) findViewById(R.id.discovered);
         connecting = (TextView) findViewById(R.id.connecting);
+        uuids = (TextView) findViewById(R.id.uuids);
         connect = (Button) findViewById(R.id.connect);
 
         // Register for broadcasts when a device is discovered.
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(receiver, filter);
+        IntentFilter discovery_filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(discovery, discovery_filter);
+
+        // Register for broadcasts when features are returned
+        IntentFilter uuid_filter = new IntentFilter(BluetoothDevice.ACTION_UUID);
+        registerReceiver(features, uuid_filter);
 
         // Get the device bluetooth adapter
         adapter = BluetoothAdapter.getDefaultAdapter();
@@ -69,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+    private final BroadcastReceiver discovery = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
 
@@ -82,8 +90,15 @@ public class MainActivity extends AppCompatActivity {
                 String deviceHardwareAddress = device.getAddress(); // MAC address
                 // Write device
                 discovered.append(deviceName + ", " + deviceHardwareAddress + "\n");
-                if(deviceHardwareAddress.equals("84:A6:C8:31:F9:F6"/*"B8:27:EB:BD:A5:BF"*/)) {
-                    adapter.cancelDiscovery();
+                if(deviceHardwareAddress.equals(/*"B8:27:EB:2E:F1:3F"*//*"B8:27:EB:BD:A5:BF"*/"84:A6:C8:31:F9:F6")) {
+                    boolean result = adapter.cancelDiscovery();
+                    if(result == true) {
+                        discovered.append("Cancelled device discovery\n");
+                    }
+                    // Get ready to requestb UUIDs
+                    uuids.setText("UUIDs for: " + device.getName() +"\n");
+                    // Start a feature request
+                    device.fetchUuidsWithSdp();
                     // Enable the connect button
                     connect.setVisibility(View.VISIBLE);
                 }
@@ -93,20 +108,56 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    // Create a BroadcastReceiver for ACTION_UUID.
+    private final BroadcastReceiver features = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_UUID.equals(action)) {
+                // Found a feature
+                if(!intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE).equals(device)) {
+                    uuids.setText("Feature returned for wrong device\n");
+                }
+                Parcelable[] u = intent.getParcelableArrayExtra(BluetoothDevice.EXTRA_UUID);
+                for(Parcelable uuid : u) {
+                    uuids.append(uuid.toString() + "\n");
+                }
+            }
+        }
+
+    };
+
     public void connect(View view) {
-        connecting.setText("Connecting to device...");
-        // UUID for server
-        UUID uuid = UUID.fromString("d364b420-8d71-11e3-baa8-0800200c9a66");
+        connecting.setText("Connecting to device: " + device.getName() + "\n");
+        try {
+            device.fetchUuidsWithSdp();
+
+        } catch (Exception e) {
+            connecting.append(e.getMessage());
+        }
+        //for (ParcelUuid uuid : uuids) {
+        //    connecting.append("UUID: " + uuid.getUuid().toString());
+        //}
+        // UUID for serial protocol
+        UUID uuid = UUID.fromString("00005005-0000-1000-8000-0002ee000001"/*"00000000-0000-0000-0000-0000cdab0000"*//*"0000110e-0000-1000-8000-00805F9B34FB"*/);
         // Connect to the device
         try {
             // Get a BluetoothSocket to connect with the given BluetoothDevice.
             // MY_UUID is the app's UUID string, also used in the server code.
-            socket = device.createInsecureRfcommSocketToServiceRecord(uuid);
+            socket = device.createRfcommSocketToServiceRecord(uuid);
+            if(socket == null) {
+                connecting.append("Failed to make socket\n");
+                return;
+            }
         } catch (IOException e) {
+            // Unable to connect; close the socket and return.
+            connecting.append(e.getMessage() + "\n");
             Log.e(TAG, "Socket's create() method failed", e);
         }
 
         try {
+            if(!socket.isConnected()) connecting.append("Socket not connected\n");
             // Connect to the remote device through the socket. This call blocks
             // until it succeeds or throws an exception.
             socket.connect();
@@ -118,6 +169,8 @@ public class MainActivity extends AppCompatActivity {
                 socket.close();
             } catch (IOException closeException) {
                 Log.e(TAG, "Could not close the client socket", closeException);
+                connecting.append("Could not close socket: "
+                        + connectException.getMessage() + "\n");
             }
             return;
         }
@@ -130,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
 
         Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
 
-        paired.append("Paired devices:\n");
+        paired.setText("Paired devices:\n");
 
         // List the paired devices
         if (pairedDevices.size() > 0) {
@@ -162,7 +215,8 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(receiver);
+        unregisterReceiver(discovery);
+        unregisterReceiver(features);
     }
 
 }
