c4f286cc25f013d44aacc08fe946d67d3e8ae4ba
John Scott <johnrscott0@gmail.com>
2019-02-28 19:27:04 +0000

    Added led and button tests

--- src/mathematics/.d/math_test.d
+++ src/mathematics/.d/math_test.d
@@ -1,1 +1,4 @@
-math_test.o: math_test.cpp state.hpp
+math_test.o: math_test.cpp state.hpp ../testing/spi.hpp \
+ ../testing/wpi.hpp ../testing/wpi.hpp ../testing/io.hpp \
+ ../testing/spi.hpp ../testing/pin_mappings.hpp ../testing/leds.hpp \
+ ../testing/io.hpp ../testing/buttons.hpp
--- src/mathematics/math_test.cpp
+++ src/mathematics/math_test.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include "state.hpp"
 
+#include "../testing/spi.hpp"
+#include "../testing/wpi.hpp"
+#include "../testing/io.hpp"
+#include "../testing/leds.hpp"
+#include "../testing/buttons.hpp"
+
+
 int main(void)
 {
-    // TIMINGS @note 2-qubit gates take half the time of singles.
+
+    Led led0({0,4}, {0,2}, {0,3});
+  Led led1({0,7}, {0,5}, {0,6});
+  Led led2({1,4}, {1,2}, {1,3});
+  Led led3({1,7}, {1,5}, {1,6});
+  
+  led0.rgb(0.0, 0, 1.0);
+  led1.rgb(0.0, 1.0, 0.0);
+  led2.rgb(1.0, 0.0, 0.0);
+  led3.rgb(1.0, 0.0, 1.0);
+  
+  getInputOutput() -> print();
+
+  Button btn0({0,2});
+  Button btn1({1,7});
+  Button btn2({1,1});
+  Button btn3({1,2});
+  Button btn4({1,0});
+  Button btn5({1,3});
+  Button btn6({0,1});
+  Button btn7({0,0});
+  
+
+
+  // TIMINGS @note 2-qubit gates take half the time of singles.
     //      0.01s/gate for 20 qubits (on Oli's laptop)
     //      0.9s/gate for 26 qubits
     //      1.8s/gate for 27
