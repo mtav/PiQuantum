129ae840458771f37eb120e7f2deb92838945f4e
John Scott <johnrscott0@gmail.com>
2019-02-28 21:21:37 +0000

    Added some more comments

--- src/testing/spi.hpp
+++ src/testing/spi.hpp
@@ -33,40 +33,6 @@
 typedef unsigned char byte;
 
 /**
- * SpiChannel class
- *
- * This class provides SPI read/write functions on a particular SPI channel
- * using the wiringPi SPI library. The channel is fixed for a particular object
- * when it is constructed, but the frequency can be modified (not sure if that's
- * very helpful). 
- *
- * Use case: as part of an SPI device class to actually read and write data to the
- * device. Other details (such as managing chip selects) would be handled by the
- * higher level class.
- *
- */
-class SpiChannel {
-private:
-  const int channel; // Pi channel Either 0 or 1
-  const int frequency; // SPI frequency
-  WiringPi wpi; // Constructing this object ensures wiringPi is setup
-  std::vector<byte> read_write(const std::vector<byte> & write);
-  
-public:
-  // Constructor
-  SpiChannel();
-  
-  // Change frequency in Hz
-  void change_frequency(int frequency);
-  
-  // read takes int returns vector
-  std::vector<byte> read(int num_bytes);
-
-  // write takes ref vector 
-  void write(const std::vector<byte> & write);
-};
-
-/**
  * @brief Wrapper to return SPI class
  *
  * @detail Return a shared_ptr object to an SPI channel.
@@ -77,5 +43,33 @@ public:
  *
  */
 std::shared_ptr<SpiChannel> getSpiChannel();
+
+/**
+ * @brief SpiChannel class
+ *
+ * @detail This class provides SPI read/write functions on a particular SPI channel
+ * using the wiringPi SPI library. The channel is fixed for a particular object
+ * when it is constructed, but the frequency can be modified (not sure if that's
+ * very helpful). 
+ *
+ * Use case: as part of an SPI device class to actually read and write data to the
+ * device. Other details (such as managing chip selects) would be handled by the
+ * higher level class.
+ *
+ */
+class SpiChannel
+{
+private:
+  const int channel; // Pi channel Either 0 or 1
+  const int frequency; // SPI frequency
+  WiringPi wpi; // Constructing this object ensures wiringPi is setup
+  std::vector<byte> read_write(const std::vector<byte> & write);
+  
+public:
+  SpiChannel(); // Constructor
+  void change_frequency(int frequency); // Change frequency in Hz
+  std::vector<byte> read(int num_bytes); // read takes int returns vector
+  void write(const std::vector<byte> & write);   // write takes ref vector  
+};
 
 #endif
