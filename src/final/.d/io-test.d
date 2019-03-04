75a3877157a961dfcf8d1a1e6acac915a8531d7f
John Scott <johnrscott0@gmail.com>
2019-03-04 14:05:27 +0000

    Moved IO stuff to final foldert

---
+++ src/final/.d/io-test.d
@@ -0,0 +1,2 @@
+io-test.o: io-test.cpp spi.hpp wpi.hpp io.hpp pin_mappings.hpp leds.hpp \
+ buttons.hpp
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "state.hpp"
+#include "final/spi.hpp"
 
 int main() 
 {
