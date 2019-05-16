b0be3896ca5899b47b482f31a43229d351c7716d
Oliver Thomas <oliver.thomas404@gmail.com>
2019-05-16 21:20:12 +0100

    Controller class working now.

---
+++ src/controller.cpp
@@ -0,0 +1,24 @@
+/***
+ * @file controller.cpp
+ * @authors O Thomas, J Scott
+ * @date May 2019
+ * @brief usb gamepad controller implementation file
+ */
+
+#include "controller.hpp"
+
+int main(void)
+{
+    std::string path = "/dev/input/js0";
+
+    // make the controller object 
+    Controller controller(path); 
+
+    while(1)
+    {
+        // read it 
+        std::cout << controller.get_btn() << std::endl;
+    }
+
+    return 0;
+}
