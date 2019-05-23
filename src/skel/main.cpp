7ec5ca20799d84a86b6f586db97b58cc5f96cb8e
John Scott <johnrscott@gmail.com>
2019-05-23 11:39:52 +0100

    Started adding a code plan

---
+++ src/skel/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @authors O Thomas, J Scott
+ * @date May 2019
+ * @brief Program entry point
+ * @detail Execute main to start the PiQuantum program
+ *
+ */
+
+int main() {
+  
+  /**
+   *
+   *
+   */
+
+
+}
